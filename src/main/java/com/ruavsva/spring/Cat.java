960f6f4e1a41f9a590a448ffdaf601abb6f4423c
MrCrabs67 <mrcrabs67forms@outlook.com>
2023-02-03 18:30:40 +0300

    introduction

--- src/main/java/com/ruavsva/spring/Cat.java
+++ src/main/java/com/ruavsva/spring/Cat.java
@@ -1,5 +1,8 @@
 package com.ruavsva.spring;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Cat implements Pet{
     public Cat(){
         System.out.println("Cat bean is created");
---
+++ src/main/java/com/ruavsva/spring/configWithAnnotations1.java
@@ -0,0 +1,14 @@
+package com.ruavsva.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class configWithAnnotations1 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext3.xml");
+
+        Cat myCat = context.getBean("cat", Cat.class);
+        myCat.Say();
+
+        context.close();
+    }
+}
